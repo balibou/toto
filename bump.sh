1f3987cf8f4f1c59230528ce5b45d1c1adbc4301
balibou <benjamin.cherion@gmail.com>
2018-01-01 12:41:36 +0100

    fix

--- bump.sh
+++ bump.sh
@@ -2,17 +2,18 @@
 
 # this script will create a hotfix branch
 
-# get package version
+# get package.json version
 prop="version"
 PACKAGE_VERSION="$(node -pe "require('./package.json')['$prop']")"
 
-# define hotfix version
-BASE_STRING=$PACKAGE_VERSION
-BASE_LIST=(`echo $BASE_STRING | tr '.' ' '`)
-V_MAJOR=${BASE_LIST[0]}
-V_MINOR=${BASE_LIST[1]}
-V_PATCH=${BASE_LIST[2]}
-echo "Current version : $BASE_STRING"
+# define package version
+PACKAGE_VERSION_LIST=(`echo $PACKAGE_VERSION | tr '.' ' '`)
+V_MAJOR=${PACKAGE_VERSION_LIST[0]}
+V_MINOR=${PACKAGE_VERSION_LIST[1]}
+V_PATCH=${PACKAGE_VERSION_LIST[2]}
+echo "Current version : $PACKAGE_VERSION"
+
+# asking for hotfix version (with suggestion)
 V_PATCH=$((V_PATCH + 1))
 SUGGESTED_VERSION="$V_MAJOR.$V_MINOR.$V_PATCH"
 read -p "Enter a version number [Suggested (press Enter): $SUGGESTED_VERSION]: " INPUT_STRING
